97844d8b832fcfba8d988c0c6998c00b38286c0f
IHsunChen <s0935959625@gmail.com>
2022-04-21 00:38:02 +0800

    updated

--- components/Layout.js
+++ components/Layout.js
@@ -6,13 +6,6 @@ import Music from '../components/Music'
 function Layout({ children }) {
   return (
     <>
-      <meta name="description" content="A brand for the metaverse, built by the community."></meta>
-      <meta name="twitter:card" content="summary_large_image"></meta>
-      <meta name="twitter:site" content="@azukizen"></meta>
-      <meta name="twitter:creator" content="@azukizen"></meta>
-      <meta name="twitter:title" content="Azuki"></meta>
-      <meta name="twitter:description" content="We are the skaters of the internet."></meta>
-      <meta name="twitter:image" content="https://azuki.com/twitterimage.jpg"></meta>
       <Header />
         {children}
       <Music />
